3646659ff15c023bc10f202f2284cdf5d580fd61
Mary Primeau <mary.primeau@uconnect.umt.edu>
2018-10-01 12:46:38 -0600

    added black hole

--- hw6_algorithmic/sketch.js
+++ hw6_algorithmic/sketch.js
@@ -1,6 +1,7 @@
 function setup() {
 // create a canvas to draw my portrait on
     createCanvas( 1000, 600 );
+    noStroke();
 
 }
 
@@ -8,7 +9,7 @@ function setup() {
 function draw() {
 // set the background color
     background('darkblue');
-    var pSize = 40; // orginal eSize
+      var pSize = 40; // orginal eSize
       var x1 = map(mouseX, 0, width, 0, 20);
       var y1 = 200;
       var x2 = sq(x1);
@@ -16,8 +17,7 @@ function draw() {
       var leftU = 5;
       var rightU = 980;
       var xK = constrain(mouseX, leftU, rightU);
-      var spaceship_x = width/3;
-      var spaceship_y = height/3;
+
 
 // Draw the edge of universe
   push();
@@ -45,13 +45,11 @@ function draw() {
       ellipse(x3 ,xK ,pSize*3);
   pop();
 
-//space ship floating
-  push();
-  fill('gray');
-  triangle(325, 200, 300, 300, 450, 350);
-// update pos
-  spaceship_x += random( -5, 5 );
-  spaceship_y += random( -5, 5 );
+//black hole
+        for (var i = 500; i < 600; i++) {
+          var r = random(-25, 25);
+          fill('black');
+          ellipse(300, i, 350 + r, i);
+        }
 
-  pop();
   }
