99ebe2c0a5fa49d5172d3c694dea319d5e61faf0
Mary Primeau <mary.primeau@uconnect.umt.edu>
2018-10-01 12:54:00 -0600

    made a weird spaceship

--- hw6_algorithmic/sketch.js
+++ hw6_algorithmic/sketch.js
@@ -1,7 +1,7 @@
 function setup() {
 // create a canvas to draw my portrait on
     createCanvas( 1000, 600 );
-    noStroke();
+  
 
 }
 
@@ -45,11 +45,13 @@ function draw() {
       ellipse(x3 ,xK ,pSize*3);
   pop();
 
-//black hole
-        for (var i = 500; i < 600; i++) {
-          var r = random(-25, 25);
-          fill('black');
-          ellipse(300, i, 350 + r, i);
+//scary spaceship
+  push();
+    for (var i = 500; i < 800; i++) {
+    var r = random(-25, 25);
+    fill('black');
+    ellipse(100, i, 100 + r, i);
+  pop();
         }
 
   }
