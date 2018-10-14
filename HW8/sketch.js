568e587671f4f843cea5b506872bd562ebee5c98
Mary Primeau <mary.primeau@uconnect.umt.edu>
2018-10-14 17:32:51 -0600

    added more varibles for trees

--- HW8/sketch.js
+++ HW8/sketch.js
@@ -9,14 +9,20 @@ function draw() {
 
     fill('rgb(2, 29, 3)');
 
+// makes a line of trees on the x value
     var X = 30;
     var V = 58;
     var G = 86;
+// makes a second line of trees up from the y value
+    var L = 550;
+    var K = 500; //center point of triangle
+    var F = 515; // center point of triangle2
+    var B = 565;
 
 while ( X <= width){
-    triangle(X, 550, V, 500, G, 550);
-    triangle(X, 565, V, 515, G, 565);
-    line(V ,565 ,V ,575 );
+    triangle(X, L, V, K, G, L);
+    triangle(X, B, V, F, G, B);
+    line(V ,B ,V ,575 );
 
     X = X + 75;
     V = V + 75;
