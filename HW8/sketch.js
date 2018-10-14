b2c9b8e7628bc7ae15366226c2dadcb8539a69fe
Mary Primeau <mary.primeau@uconnect.umt.edu>
2018-10-14 17:27:21 -0600

    added one row of trees

--- HW8/sketch.js
+++ HW8/sketch.js
@@ -13,12 +13,14 @@ function draw() {
     var V = 58;
     var G = 86;
 
+while ( X <= width){
     triangle(X, 550, V, 500, G, 550);
     triangle(X, 565, V, 515, G, 565);
     line(V ,565 ,V ,575 );
 
-    X = X + 50;
-    V = V + 50;
-    G = G + 50;
+    X = X + 75;
+    V = V + 75;
+    G = G + 75;
+}
 
 }
