a45514791ece024a38b27d44827f62744277bac4
Mary Primeau <mary.primeau@uconnect.umt.edu>
2018-10-14 17:20:56 -0600

    added var for trees

--- HW8/sketch.js
+++ HW8/sketch.js
@@ -8,7 +8,17 @@ function draw() {
 
 
     fill('rgb(2, 29, 3)');
-    triangle(30, 550, 58, 500, 86, 550);
-    triangle(30, 565, 58, 515, 86, 565);
-    line(58 ,565 ,58 ,575 );
+
+    var X = 30;
+    var V = 58;
+    var G = 86;
+
+    triangle(X, 550, V, 500, G, 550);
+    triangle(X, 565, V, 515, G, 565);
+    line(V ,565 ,V ,575 );
+
+    X = X + 50;
+    V = V + 50;
+    G = G + 50;
+
 }
