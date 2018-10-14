542a5bf26b11e9872554a1a8e7f75594434a9b49
Mary Primeau <mary.primeau@uconnect.umt.edu>
2018-10-14 17:13:15 -0600

    drew one tree

---
+++ HW8/sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+    createCanvas(windowWidth, 600);
+    strokeWeight(2);
+}
+
+function draw() {
+    background('rgb(10, 9, 51)');
+
+
+    fill('rgb(2, 29, 3)');
+    triangle(30, 550, 58, 500, 86, 550);
+    triangle(30, 565, 58, 515, 86, 565);
+    line(58 ,565 ,58 ,575 );
+}
