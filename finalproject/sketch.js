6f658150cccdc8e5beb47ea7e32fc12cc06cb719
Mary Primeau <mary.primeau@uconnect.umt.edu>
2018-11-27 11:32:36 -0700

    updated forest animation

--- finalproject/sketch.js
+++ finalproject/sketch.js
@@ -2,6 +2,8 @@ let bg1;
 let bg2;
 let bg3;
 let bg4;
+let forest;
+
 function preload(){
   bg1 = loadImage("./images/background1.jpg")
   bg2 = loadImage("./images/background2.jpg")
@@ -20,8 +22,55 @@ function draw() {
   var fr = frameRate();
   background( 'black' );
 // change image every frame
-  image(bg1,10,10);
-  image(bg2,10,10);
-  image(bg3,10,10);
-  image(bg4,10,10);
+  forest.display();
+  forest.animation();
+
+}
+**********************************************
+************** Forest Class ******************
+**********************************************
+class forest {
+  constructor(x,y,size){
+    this.bg1 = bg1;
+    this.bg2 = bg2;
+    this.bg3 = bg3;
+    this.bg4 = bg4;
+    this.speed = 4;
+    this.posx = 10;
+    this.posy = 10;
+    this.orientation = ;
+    this.size = {
+      w:600,
+      h:400
+    };
+    this.sprite_num =0;
+// top left corner of the sprite
+    this.subrect =[
+
+      [600,10]
+    ];
+
+  }
+  display(){
+    push();
+    translate(this.pos.x, this.pos.y);
+
+        if (this.orientation === 0) {
+            scale(1,1);
+        } else if (this.orientation === 1) {
+            scale(-1,1);
+        }
+
+  image( this.bg1,0,0,this.size.w,
+    this.size.h,this.subRect[this.sprite_num][0], this.subRect[this.sprite_num][1], this.size.w, this.size.h)
+  );
+  pop();
+  }
+  animate() {
+// update the sprite num
+// This adjusts the subrectangle positions
+    if (frameCount % this.speed === 0) {
+        this.sprite_num++;
+        this.sprite_num %= 4;
+    }
 }
