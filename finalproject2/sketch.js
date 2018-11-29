c84a643814b4af0800164de67d3589f0d4c97bc5
Mary Primeau <mary.primeau@uconnect.umt.edu>
2018-11-29 11:53:12 -0700

    finshed background

--- finalproject2/sketch.js
+++ finalproject2/sketch.js
@@ -1,14 +1,12 @@
-let bg1;
-let bg2;
-let bg3;
-let bg4;
-let forest;
 
+let images = [];
+let imIdx = 0;
+//
 function preload(){
-  bg1 = loadImage("./images/background1.jpg")
-  bg2 = loadImage("./images/background2.jpg")
-  bg3 = loadImage("./images/background3.jpg")
-  bg4 = loadImage("./images/background4.jpg")
+  images[0] = loadImage("./images/background1.jpg")
+  images[1] = loadImage("./images/background2.jpg")
+  images[2] = loadImage("./images/background3.jpg")
+  images[3] = loadImage("./images/background4.jpg")
 
 }
 function setup() {
@@ -20,6 +18,9 @@ function setup() {
 
 function draw() {
   var fr = frameRate();
-  background(random(bg1,bg2,bg3,bg4));
+  background(images[imIdx]);
+
+  imIdx++
+  imIdx = imIdx % 4;
 
 }
