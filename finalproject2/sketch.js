7292665342b133e88bd01cf6a7e861228944a8db
Mary Primeau <mary.primeau@uconnect.umt.edu>
2018-12-02 12:50:52 -0700

    added basic snow function

--- finalproject2/sketch.js
+++ finalproject2/sketch.js
@@ -1,7 +1,9 @@
 
 let images = [];
 let imIdx = 0;
-//
+//snow colors
+let colors = ['rgb(236, 116, 230)', 'rgb(115, 235, 235)', 'rgb(255, 156, 149)', 'rgb(255, 255, 255)', 'rgb(195, 255, 219)'];
+
 function preload(){
   images[0] = loadImage("./images/background1.jpg")
   images[1] = loadImage("./images/background2.jpg")
@@ -18,9 +20,30 @@ function setup() {
 
 function draw() {
   var fr = frameRate();
+//make background images of forest
   background(images[imIdx]);
 
+// makes the background image repeat
   imIdx++
   imIdx = imIdx % 4;
 
+//snow function
+
+  snow();
 }
+// begin new function snow
+// snow moving with mouse functions
+function snow(){
+  let density = map(mouseX, 0, width, 20, 60);
+  fill(random(colors));
+  stroke(random(colors));
+// Make a loop of moving snow with mouse
+  for (let k = 5; k <= width-10; k += density) {
+    for (let h = 5; h <= height-10; h+=density) {
+// draw snow
+  ellipse(k , h ,5 );
+
+    }
+  }
+}
+// End snow falling
