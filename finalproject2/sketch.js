11733a6a6e3dcc2dfb64adb321500a97430c79d7
Mary Primeau <mary.primeau@uconnect.umt.edu>
2018-12-03 21:31:52 -0700

    finished shaped frog

--- finalproject2/sketch.js
+++ finalproject2/sketch.js
@@ -3,7 +3,7 @@ let images = [];
 //helps makes the background repeat
 let imIdx = 0;
 //snow colors
-let colors = ['rgb(238, 185, 207)', 'white', 'rgb(179, 181, 227)', 'rgb(164, 218, 219)', 'rgb(232, 201, 201)'];
+let colors = ['rgb(238, 185, 207)','white', 'rgb(179, 181, 227)', 'rgb(164, 218, 219)', 'rgb(232, 201, 201)'];
 
 function preload(){
 //moving forest images
@@ -62,20 +62,20 @@ function draw() {
 // begin new function snow
 // snow moving with mouse functions
 function snow(pos_L, pos_M, scale_F, scale_C){
-  // scale and position
+
+// scale and position
   translate( pos_L, pos_M );
   scale( scale_F, scale_C );
-
+push();
+  strokeWeight(0);
  let density = map(mouseX, 0, width, 15, 100);
-  fill(random(colors));
-  strokeWeight(.5);
-  stroke(random(colors));
+  fill('white');
 // Make a loop of moving snow with mouse
   for (let k = 1; k <= width - 5; k += density) {
    for (let h = 1; h <= height - 5; h +=density) {
 // draw snow
   ellipse(k ,h, 5 ,5 );
-
+pop();
     }
   }
 }
@@ -94,9 +94,31 @@ function frog (){
   stroke('black');
   strokeWeight(4);
   fill('rgb(19, 66, 17)');
+  ellipse(65 ,240, 50, 50);
   ellipse(50 ,225, 50, 50);
+  ellipse(75,250,25,25);
   pop();
 
+//frog eyes outer
+  push();
+  stroke('black');
+  strokeWeight(2);
+  fill('white');
+  ellipse(37,200,20,20);
+  ellipse(58,200,20,20);
+  pop();
+//frog eyes inner
+  push();
+  stroke('black');
+  strokeWeight(2);
+  fill('black');
+  ellipse(37,200,10,10);
+  ellipse(58,200,10,10);
+  pop();
+// frog mouth
+  push();
+  arc(50, 238, 15, 15, 0, PI + QUARTER_PI, PIE);
+  pop();
 
 }
 
@@ -128,13 +150,13 @@ function squirel (){
 ////////////////////////flowers function////////////////////////////////
 ////////////////////////////////////////////////////////////////////////
 function flowers (pos_F, pos_C, scale_F, scale_C){
-
+push();
 // scale and position
   translate( pos_F, pos_C );
   scale( scale_F, scale_C );
 
 //flower center
-  push();
+
   stroke('black');
   strokeWeight(0);
   fill('rgb(172, 154, 33)');
