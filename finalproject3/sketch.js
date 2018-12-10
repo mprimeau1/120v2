726a0dd06b366f6428941204b0cf06d77090caea
Mary Primeau <mary.primeau@uconnect.umt.edu>
2018-12-09 23:10:49 -0700

    just keep chugging along

--- finalproject3/sketch.js
+++ finalproject3/sketch.js
@@ -19,18 +19,17 @@ function preload(){
   images[2] = loadImage("./images/background3.jpg")
   images[3] = loadImage("./images/background4.jpg")
 // sounds
-//  soundWater = loadSound("./sounds/water.mp3");
-//  soundSnow = loadSound("./sounds/snow.mp3");
-//  soundWhistle = loadSound("./sounds/whisle.mp3");
-//  soundFrog = loadSound("./sounds/frog.mp3");
-//  soundBird = loadSound("./sounds/bird.mp3");
-//  soundSquirrel = loadSound("./sounds/squirel.wav");
+  soundWater = loadSound("./sounds/water.mp3");
+  soundSnow = loadSound("./sounds/snow.mp3");
+  soundWhistle = loadSound("./sounds/whisle.mp3");
+  soundFrog = loadSound("./sounds/frog.mp3");
+  soundBird = loadSound("./sounds/bird.mp3");
+  soundSquirrel = loadSound("./sounds/squirel.wav");
 }
 function setup() {
 // set frame rate 4 frames second
   frameRate(4);
     createCanvas( 600,400 );
-    soundWater = loadSound("./sounds/water.mp3",loaded);
 // create a new snow object of class type "snowFlake"
       let init_x = 20;
       let init_y = 60;
@@ -44,9 +43,7 @@ function setup() {
               }
           }
 }
-function loaded(){
-  soundWater.play();
-}
+
 
 function draw() {
   noCursor();
